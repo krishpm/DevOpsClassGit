48f30f08632ace7af9471c94b0b14542dbe32c69
Padmini Krishnamurthy <krishpm@gmail.com>
2019-10-25 16:05:42 -0700

    Added prints

--- devops01a.sh
+++ devops01a.sh
@@ -1,11 +1,16 @@
-# number of volumes, size of each volume
-fdisk -l
-#free space on each volume
-df -h
-#linux number of cpus cores
-grep -c ^processor /proc/cpuinfo
-#amount of ram
-free -g 
-#ipaddress and mac address
-ifconfig -a
+##Assignment 00: Bash, Git, and GitHub
 
+echo "number of volumes, size of each volume"
+fdisk -l
+echo "#####################################"
+echo "free space on each volume"
+df -h
+echo "#####################################"
+echo "number of cpus/ cores"
+grep -c ^processor /proc/cpuinfo
+echo "#####################################"
+echo "amount of RAM"
+free -g 
+echo "#####################################"
+echo "ipaddress and mac address"
+ifconfig -a
